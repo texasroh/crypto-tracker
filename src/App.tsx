4ec779779ab566d3af2f6142438fd71951925132
June Roh <june.roh@streetsmarket.com>
2022-08-23 16:32:29 -0400

    initial router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Router from "./Router";
+
 function App() {
-  return <div></div>;
+    return <Router />;
 }
 
 export default App;
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Coin from "./routes/Coin";
+import Coins from "./routes/Coins";
 
-function Router() {}
+function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Coins />}></Route>
+                <Route path="/:coinId" element={<Coin />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
 export default Router;
