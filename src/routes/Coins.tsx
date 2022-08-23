32ad7a263d3d6a2ee2816cc855a8dc2f145b4c8a
June Roh <june.roh@streetsmarket.com>
2022-08-23 17:30:23 -0400

    set global css reset

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Header = styled.header``;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li``;
+
+const Title = styled.h1`
+    color: ${(props) => props.theme.accentColor};
+`;
+
 function Coins() {
-  return <h1>Coins</h1>;
+    return (
+        <Container>
+            <Header>
+                <Title>Coins</Title>
+            </Header>
+            <CoinsList>
+                <Coin></Coin>
+            </CoinsList>
+        </Container>
+    );
 }
 
 export default Coins;
