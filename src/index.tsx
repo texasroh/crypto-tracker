b57fc1e81642b0ef40ea2c6c61abad3ed513b132
June Roh <texasroh@gmail.com>
2022-08-27 05:11:45 -0400

    add theme toggle button

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { theme } from "./theme";
-import { ThemeProvider } from "styled-components";
+
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 const queryClient = new QueryClient();
 
 root.render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <ThemeProvider theme={theme}>
-                <App />
-            </ThemeProvider>
-        </QueryClientProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
